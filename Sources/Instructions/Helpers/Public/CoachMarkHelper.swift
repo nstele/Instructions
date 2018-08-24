12840560ccf2497c58a113dee204211b018eb5eb
Natalia Stele <stelenatalia@gmail.com>
2018-08-24 11:37:57 -0300

    Rounded distance

--- Sources/Instructions/Helpers/Public/CoachMarkHelper.swift
+++ Sources/Instructions/Helpers/Public/CoachMarkHelper.swift
@@ -173,7 +173,7 @@ public class CoachMarkHelper {
             bezierPath = makeCutoutPathWithFrame(convertedFrame)
         } else {
             if let corners = roundedCorners {
-                bezierPath = UIBezierPath(roundedRect: convertedFrame.insetBy(dx: -3, dy: -3),
+                bezierPath = UIBezierPath(roundedRect: convertedFrame.insetBy(dx: -2, dy: -2),
                                           byRoundingCorners: .allCorners,
                                           cornerRadii: CGSize(width: corners, height: corners))
             } else {
